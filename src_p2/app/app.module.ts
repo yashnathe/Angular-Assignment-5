98eecb314c8d6d528c77d6a9488551a6781f3b91
yashnathe <yashnathe360@gmail.com>
2023-04-03 23:42:09 +0530

    first commit

--- src_p2/app/app.module.ts
+++ src_p2/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstCompComponent } from './first-comp/first-comp.component';
+import { SecondCompComponent } from './second-comp/second-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstCompComponent,
+    SecondCompComponent
   ],
   imports: [
     BrowserModule
